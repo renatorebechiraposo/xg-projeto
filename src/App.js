b9406840c5b733a31e2e1e90ded75de0d8929e4f
XeydGel <renatorebechiraposo@outlook.com>
2024-11-25 22:31:56 -0300

    ✨Feat: Adicionando State de lista de tarefas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const message = "Hello World";
-  return <h1>{message}</h1>;
+  const [task, setTask] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      completed: true,
+    },
+    {
+      id: 2,
+      title: "Estudar Vue",
+      completed: false,
+    },
+  ]);
+  return (
+    <>
+      <div className="container">
+        <h1>Minhas Tarefas</h1>
+      </div>
+    </>
+  );
 };
 
 export default App;
